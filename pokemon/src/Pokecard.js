1adebaccd88c7a4d553eefc60631ae8a0137ae24
Benjamin Lavian <benylavian@gmail.com>
2020-12-25 15:35:05 -0500

    rendered pokecard

---
+++ pokemon/src/Pokecard.js
@@ -0,0 +1,9 @@
+import React,{Component} from 'react'
+
+class Pokecard extends Component{
+    // eslint-disable-next-line react/require-render-return
+    render(){
+        return <h1>POKECARD</h1>
+    }
+}
+export default Pokecard
