7ab11a197e77c975d1be0cab3060059458585da8
Benjamin Lavian <benylavian@gmail.com>
2020-12-27 15:20:54 -0500

    pokegame component

--- pokemon/src/Pokedex.js
+++ pokemon/src/Pokedex.js
@@ -20,6 +20,8 @@ class Pokedex extends Component {
       return (
           <div className="Pokedex">
               <h1>Pokedex!</h1>
+              <p> Total Experience: {this.props.exp}</p>
+              <p> {this.props.isWinner ? 'Winner' : 'Loser'}</p>
               <div className="Pokedex-cards">
               {this.props.pokemon.map((p)=>( 
               <PokeCard id={p.id} name={p.name} type={p.type} exp={p.base_experience}/>
